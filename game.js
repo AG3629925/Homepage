975d734de68d1be683290463644c3ad60e0ff92b
AG3629925 <e3629925@student.dcccd.edu>
2024-10-26 11:51:35 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
+/* Question Multi-dimensional array */
+var questions = [
+   ["In what country did the first Starbucks open outside of North America?", [3], "China", "UK", "Japan"],
+   ["When was the internet invented?", [1], "1980", "1969", "1995"],
+   ["When did Facebook first launch?", [2], "2003", "2005", "2004"]
+];
+var questionCounter = 0;
 
+function startGame() {
+  if(questions.length > 0) {
+    var currentQuestion = questions.shift();
+    displayQuestion(currentQuestion);
+    questionCounter++;
+  } else {
+    document.getElementById("prompt").innerHTML = `<button
+  }    
+}  
+function displayQuestion(question) {
+  document.getElementById("question").innerText = currentQuestion[0];
+
+  var answerList = '';
+  for (var i = 2; i < question.length; i++) {
+    answersList += 
+    
+  
