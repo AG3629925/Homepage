a5c042ae8d97a9450d3d852e9d5a6a6023bb54fd
AG3629925 <e3629925@student.dcccd.edu>
2024-10-27 20:44:25 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
-// Multi-dimensional array to store questions, correct answer index, and possible answers
 var questions1 = [
     ["In what country did the first Starbucks open outside of North America?", 2, "China", "UK", "Japan"],
     ["When was the internet invented?", 1, "1980", "1969", "1995"],
     ["When did Facebook first launch?", 2, "2003", "2005", "2004"]
 ];
-var questions = [...questions1]
+var questions = [...questions1];
 var questionCounter = 0;
 
 function playGame() {
@@ -21,7 +20,6 @@ function playGame() {
 
 function displayQuestion(question) {
     document.getElementById("question").innerText = question[0];
-
     var answerList = '';
     for (var i = 2; i < question.length; i++) {
         answerList += `<li><button onclick="checkAnswer(${i-2}, ${question[1]})">${question[i]}</button></li>`;
@@ -36,11 +34,10 @@ function checkAnswer(selectedIndex, correctIndex) {
         alert("Incorrect!");
     }
     playGame();
-
 }
 
 function restartGame() {
-    questions = [...questions1];
+    questions = [...questions1]; 
     questionCounter = 0;
     playGame();
 }
