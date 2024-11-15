90b09b70da7693f15f02fb1a6014ba281fcb6fc1
AG3629925 <e3629925@student.dcccd.edu>
2024-11-14 21:17:02 -0600

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,1 +1,30 @@
+$(document).ready(function() {
+  $('#newsSignup').hide();
 
+  $('#signuplink').click(function(e) {
+    e.preventDefault();
+    $('#newsSignup').slideToggle();
+    var $openclose = $('#openclose');
+    $openclose.text($openclose.text() === '+' ? '-' : '+');
+  });
+
+  $('#slogan').hover(
+      function() {
+        $(this).fadeOut('normal', 'linear', function() {
+          $(this).text('Hand Picked Just For You').fadeIn('slow', 'swing');
+        });
+      },
+      function() {
+        $(this).fadeOut('fast', 'swing', function() {
+          $(this).text('The Power of Flowers'').fadeIn('slow', 'linear');
+      });
+    });
+    $('#rose').animate({right: '100px', opacity:1}, 'slow', 'swing');
+   
+    $('#newsSignup').submit(function(e) {
+      e.preventDEfault();
+      alert('Thank you for registering');
+      $(this).hide();
+      $('#signuplink').fadeTo('slow', 0.3);
+    });
+)};
