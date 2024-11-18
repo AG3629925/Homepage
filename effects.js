1212be5d3c57b53de1be7b892e2a02ff87249b83
AG3629925 <e3629925@student.dcccd.edu>
2024-11-17 20:39:03 -0600

    Update effects.js

--- effects.js
+++ effects.js
@@ -13,22 +13,24 @@ $(document).ready(function() {
   });
 
   $('#slogan').hover(
-      function() {
-        $(this).fadeOut('normal', 'linear', function() {
-          $(this).text('Hand Picked Just For You').fadeIn('slow', 'swing');
-        });
-      },
-      function() {
-        $(this).fadeOut('fast', 'swing', function() {
-          $(this).text('The Power of Flowers').fadeIn('slow', 'linear');
+    function() {
+      $(this).fadeOut('normal', 'linear', function() {
+        $(this).text('Hand Picked Just For You').fadeIn('slow', 'swing');
       });
-    });
-    $('#rose').animate({right: '100px', opacity:1}, 'slow', 'swing');
-   
-    $('#newsSignup').submit(function(event) {
-      event.preventDefault();
-      alert('Thank you for registering');
-      $(this).hide();
-      $('#signuplink').fadeTo('slow', 0.3);
-    });
-)};
+    },
+    function() {
+      $(this).fadeOut('fast', 'swing', function() {
+        $(this).text('The Power of Flowers').fadeIn('slow', 'linear');
+      });
+    }
+  );
+
+  $('#rose').animate({ right: '100px', opacity: 1 }, 'slow', 'swing');
+
+  $('#newsSignup').submit(function(event) {
+    event.preventDefault();
+    alert('Thank you for registering');
+    $(this).hide();
+    $('#signuplink').fadeTo('slow', 0.3);
+  });
+});
