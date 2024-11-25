0f9e6489062b207b5d1339ad740114ffd3d5fd87
AG3629925 <e3629925@student.dcccd.edu>
2024-11-24 22:36:44 -0600

    Update shopping.js

--- shopping.js
+++ shopping.js
@@ -1,1 +1,33 @@
+$(document).ready(function () {
+  var itemCount = 0;
+  var removeButton  = '<span class="del">Remove</span>';
 
+  $('.add').click(function () {
+    itemCount++;
+    $('#empty').hide();
+
+    var itemName = $(this).siblings('.name').text();
+    var itemID = $(this).siblings('.name').attr('id');
+
+    var cartLink = "<li class='cartItem' name='" + itemID + "'>" + itemName + " " + removeButton + "</li>";
+    $('#cart').append(cartLink);
+
+    $(this).hide();
+  });
+
+  $('#cart').on('click', '.del', function () {
+    $(this).parent().remove();
+    itemCount--;
+    if (itemCount === 0) {
+      $('#empty').show();
+    }
+    var itemID = $(this).parent().attr('name');
+    $('#' + itemID).siblings('.add').show();
+  });
+
+  $('.rating').on('click', '.star', function () {
+    var $this = $(this);
+    $this.siblings().attr('src', 'staroff.gif');
+    $this.attr('src', 'staron.gif').prevAll().attr('src', 'staron.gif');
+  });
+});
