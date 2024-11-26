8d9ed2a9281cadc9130b65e27d56c20a700bf3a6
AG3629925 <e3629925@student.dcccd.edu>
2024-11-25 22:29:23 -0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $(".thumb").click(function() {
     let newSrc = $(this).attr("src");
     let newAlt = $(this).attr("alt");
-    $("#largeImage").attr("src", newSrc).attr("alt", newAlt);
+    $("#lgPic").attr("src", newSrc).attr("alt", newAlt);
     $("#imageCaption").text(newAlt);
   });
 
-  $("#largeImage").click(function() {
+  $("#lgPic").click(function() {
     let src = $(this).attr("src");
     window.open(src, '_blank');
   });
